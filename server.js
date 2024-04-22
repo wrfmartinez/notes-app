efd2924a45ec606de885b2286ba21e94e28bee02
wrfmartinez <wrmartinez709@gmail.com>
2024-04-22 14:56:51 -0400

    Fix staff profile pages

--- server.js
+++ server.js
@@ -19,109 +19,104 @@ const testimonials = [
 ];
 
 const staffMem = [
-    {
-      name: "Michael Roberts",
-      nickname: `Visionary`,
-      position: `CEO`,
-      favFeature: `Tagging system for organizing and quickly finding specific notes`,
-      bio: `As the CEO, Michael provides the overarching vision and direction for the note-taking app company. With his leadership skills and business acumen, he sets ambitious goals and leads the team toward achieving them. Michael fosters a culture of innovation and collaboration, empowering team members to strive for excellence.`,
-    },
-    {
-        name: "Jennifer Wu",
-        nickname: `TechGenius`,
-        position: `CTO (Chief Technology Officer)`,
-        favFeature: `Optical Character Recognition (OCR) for scanning and digitizing text from images in notes`,
-        bio: `Jennifer, the CTO, is the technical genius behind the note-taking app's architecture and development process. With her deep knowledge of technology and software development best practices, she drives the technical strategy and ensures that the app is built to scale and perform flawlessly. Jennifer is constantly exploring new technologies and methodologies to keep the app ahead of the curve.`,
-      },
-      {
-        name: "Ryan Lee",
-        nickname: `MarketMaestro`,
-        position: `Marketing Manager`,
-        favFeature: `Audio recording with synchronized note-taking for capturing meeting discussions`,
-        bio: `Ryan is the mastermind behind the marketing strategy for the note-taking app. With his creative ideas and strategic thinking, he drives user acquisition, engagement, and retention through various channels. Ryan keeps a pulse on market trends and user feedback to tailor marketing efforts and ensure the app's success in a competitive landscape.`,
-      },
-      {
-        name: "Alex Johnson",
-        nickname: `SupportWhiz`,
-        position: `Customer Support Specialist`,
-        favFeature: `Handwriting recognition for converting handwritten notes to digital text`,
-        bio: `Alex is the friendly face behind customer support for the note-taking app. With his patient demeanor and technical expertise, he assists users with any questions or issues they encounter while using the app. Alex goes above and beyond to ensure that every user has a positive experience and gets the help they need.`,
-      },
-      {
-        name: "Emily Nguyen",
-        nickname: `ProdMaster`,
-        position: `Product Manager`,
-        favFeature: `Integration with cloud storage platforms for seamless access across devices`,
-        bio: `As the Product Manager, Emily oversees the development and enhancement of the note-taking app. She is responsible for setting the product roadmap, prioritizing features, and ensuring that the app meets the needs of its users. Emily works closely with the development team, designers, and other stakeholders to deliver a product that exceeds expectations.`,
-      },
-      {
-        name: "David Patel",
-        nickname: `DesignGuru`,
-        position: `UX/UI Designer`,
-        favFeature: `Customizable color themes for organizing notes`,
-        bio: `David is the creative force behind the user interface and experience of the note-taking app. His eye-catching designs and intuitive user interfaces make the app visually appealing and easy to navigate. With a keen understanding of user behavior, David ensures that every interaction with the app is smooth and intuitive`,
-      },
-      {
-        name: "Sarah Chang",
-        nickname: `CodeSavvy`,
-        position: `Senior Software Engineer`,
-        favFeature: `Markdown support for formatting notes`,
-        bio: `As a Senior Software Engineer, Sarah is the backbone of the development team. With her expertise in coding and problem-solving skills, she ensures that the note-taking app's software is robust, efficient, and bug-free. She's passionate about optimizing the app's performance and enhancing its features for a seamless user experience.`,
-      },
-  ];
-  app.get('/staff/:member', (req, res) => {
-    const mem = req.params.member //members are categorized by their nicknames
-    let membs = [] // each decalered object property will go here 
-    staffMem.forEach(element => {
-        
-        if (mem === "Visionary") {
-                
-                // membs.push(element)
-                console.log(element);
-                
-            } else
-        if (mem === "TechGenius") {
-                
-                // membs.push(element)
-                // console.log();
-                
-            } 
+  {
+    name: "Michael Roberts",
+    nickname: `Visionary`,
+    position: `CEO`,
+    favFeature: `Tagging system for organizing and quickly finding specific notes`,
+    bio: `As the CEO, Michael provides the overarching vision and direction for the note-taking app company. With his leadership skills and business acumen, he sets ambitious goals and leads the team toward achieving them. Michael fosters a culture of innovation and collaboration, empowering team members to strive for excellence.`,
+  },
+  {
+    name: "Jennifer Wu",
+    nickname: `TechGenius`,
+    position: `CTO (Chief Technology Officer)`,
+    favFeature: `Optical Character Recognition (OCR) for scanning and digitizing text from images in notes`,
+    bio: `Jennifer, the CTO, is the technical genius behind the note-taking app's architecture and development process. With her deep knowledge of technology and software development best practices, she drives the technical strategy and ensures that the app is built to scale and perform flawlessly. Jennifer is constantly exploring new technologies and methodologies to keep the app ahead of the curve.`,
+  },
+  {
+    name: "Ryan Lee",
+    nickname: `MarketMaestro`,
+    position: `Marketing Manager`,
+    favFeature: `Audio recording with synchronized note-taking for capturing meeting discussions`,
+    bio: `Ryan is the mastermind behind the marketing strategy for the note-taking app. With his creative ideas and strategic thinking, he drives user acquisition, engagement, and retention through various channels. Ryan keeps a pulse on market trends and user feedback to tailor marketing efforts and ensure the app's success in a competitive landscape.`,
+  },
+  {
+    name: "Alex Johnson",
+    nickname: `SupportWhiz`,
+    position: `Customer Support Specialist`,
+    favFeature: `Handwriting recognition for converting handwritten notes to digital text`,
+    bio: `Alex is the friendly face behind customer support for the note-taking app. With his patient demeanor and technical expertise, he assists users with any questions or issues they encounter while using the app. Alex goes above and beyond to ensure that every user has a positive experience and gets the help they need.`,
+  },
+  {
+    name: "Emily Nguyen",
+    nickname: `ProdMaster`,
+    position: `Product Manager`,
+    favFeature: `Integration with cloud storage platforms for seamless access across devices`,
+    bio: `As the Product Manager, Emily oversees the development and enhancement of the note-taking app. She is responsible for setting the product roadmap, prioritizing features, and ensuring that the app meets the needs of its users. Emily works closely with the development team, designers, and other stakeholders to deliver a product that exceeds expectations.`,
+  },
+  {
+    name: "David Patel",
+    nickname: `DesignGuru`,
+    position: `UX/UI Designer`,
+    favFeature: `Customizable color themes for organizing notes`,
+    bio: `David is the creative force behind the user interface and experience of the note-taking app. His eye-catching designs and intuitive user interfaces make the app visually appealing and easy to navigate. With a keen understanding of user behavior, David ensures that every interaction with the app is smooth and intuitive`,
+  },
+  {
+    name: "Sarah Chang",
+    nickname: `CodeSavvy`,
+    position: `Senior Software Engineer`,
+    favFeature: `Markdown support for formatting notes`,
+    bio: `As a Senior Software Engineer, Sarah is the backbone of the development team. With her expertise in coding and problem-solving skills, she ensures that the note-taking app's software is robust, efficient, and bug-free. She's passionate about optimizing the app's performance and enhancing its features for a seamless user experience.`,
+  },
+];
 
+app.get('/staff/:member', (req, res) => {
+  const mem = req.params.member //members are categorized by their nicknames
+  let membs = [] // each decalered object property will go here
+  staffMem.forEach(element => {
 
+    if (mem === "Visionary" && element.nickname === "Visionary") {
+      membs.push(element)
+      console.log(element);
+    }
 
+    if (mem === "TechGenius" && element.nickname === "TechGenius") {
+      membs.push(element)
+    }
+    if (mem === "MarketMaestro" && element.nickname === "MarketMaestro") {
+      membs.push(element)
+    }
+    if (mem === "SupportWhiz" && element.nickname === "SupportWhiz") {
+      membs.push(element)
+    }
+    if (mem === "ProdMaster" && element.nickname === "ProdMaster") {
+      membs.push(element)
+    }
+    if (mem === "DesignGuru" && element.nickname === "DesignGuru") {
+      membs.push(element)
+    }
+    if (mem === "CodeSavvy" && element.nickname === "CodeSavvy") {
+      membs.push(element)
+    }
+  });
 
+  res.render('member.ejs', {
 
-
-
-
-    
-    });
-                                        
-    
-res.render('member.ejs', {
-    
-            name: membs.name,
-            nickname: membs.nickname,
-            position: membs.position,
-            feature: membs.favFeature,
-            bio: membs.bio,
-          });
-
-
-
-
-
-
-
+    name: membs[0].name,
+    nickname: membs[0].nickname,
+    position: membs[0].position,
+    feature: membs[0].favFeature,
+    bio: membs[0].bio,
   })
+})
 
 
 
-  app.get('/staff', (req, res) => {
-    res.render('staff.ejs', {
-      staffList: staffMem
-    });
-  })
+app.get('/staff', (req, res) => {
+  res.render('staff.ejs', {
+    staffList: staffMem
+  });
+})
 
 
 app.get('/testimonials', (req, res) => {
@@ -146,47 +141,47 @@ app.listen(3000);
 // if (mem === "Visionary") {
 //     staffMem.forEach(element => {
 //         membs.push(element)
-        
+
 //     });
-//     } 
+//     }
 //     if (mem === "TechGenius") {
 //         staffMem.forEach(element => {
 //             membs = []
 //             membs.push(element)
 //         });
-//         } 
+//         }
 //     if (mem === "MarketMaestro") {
 //     staffMem.forEach(element => {
 //         membs = []
 //         membs.push(element)
-        
+
 //     });
-//     } 
+//     }
 //     if (mem === "SupportWhiz") {
 //         staffMem.forEach(element => {
 //             membs = []
 //             membs.push(element)
-            
+
 //         });
-//         } 
+//         }
 //         if (mem === "ProdMaster") {
 //             staffMem.forEach(element => {
 //                 membs = []
 //                 membs.push(element)
-                
+
 //             });
-//             } 
+//             }
 //             if (mem === "DesignGuru") {
 //                 staffMem.forEach(element => {
 //                     membs = []
 //                     membs.push(element)
-                    
+
 //                 });
-//                 } 
+//                 }
 //                 if (mem === "CodeSavvy") {
 //                     staffMem.forEach(element => {
 //                         membs = []
 //                         membs.push(element)
-                        
+
 //                     });
-//                     } 
+//                     }
