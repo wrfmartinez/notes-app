e9077abfbc1993d16338c90688f866dd25b55828
wrfmartinez <wrmartinez709@gmail.com>
2024-04-30 17:58:18 -0400

    Add heroku PORT listen

--- server.js
+++ server.js
@@ -187,4 +187,7 @@ app.get('/', (req, res) => {
   })
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
